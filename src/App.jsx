8c7f982354188d56c60ffaf20ca1f62682233d64
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-14 19:22:09 +0600

    special setup

--- src/App.jsx
+++ src/App.jsx
@@ -12,42 +12,69 @@ let auth = getAuth(app);
 function App() {
   let [email, setEmail] = useState('');
   let [password, setPassword] = useState('');
-
+  const [validated, setValidated] = useState(false);
+  let [error, setError] = useState('')
+  //Blur input field functions
   let emailBlur = event => {
     setEmail(event.target.value)
   }
   let passBlur = event => {
     setPassword(event.target.value)
   }
+  // Form submit functions
   let formSubmit = event => {
+
+    // Form validation
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    setValidated(true);
+
+    // Case checking 
+    if (!/(?=.*[!@#$%^&*])/.test(password)) {
+      setError('Password should contain at least one special character')
+      return;
+    }
+
+    // Send to the firebase server
     createUserWithEmailAndPassword(auth, email, password)
-    .then(result =>{
-      let user = result.user;
-      console.log(user);
-    })
-    .catch(error =>{
-      console.log(error);
-    });
+      .then(result => {
+        let user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+        console.log(error);
+      });
     console.log('submitted', email, password)
     event.preventDefault();
   }
 
   return (
     <div>
-      <Form onSubmit={formSubmit}>
+      <Form noValidate validated={validated} onSubmit={formSubmit}>
         <h2 className='text-primary'>Input your information</h2>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control onBlur={emailBlur} type="email" placeholder="Enter email" />
+          <Form.Control onBlur={emailBlur} type="email" placeholder="Enter email" required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control onBlur={passBlur} type="password" placeholder="Password" />
+          <Form.Control onBlur={passBlur} type="password" placeholder="Password" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
         </Form.Group>
+        <p>{error}</p>
         <Button variant="primary" type="submit">
           Submit
         </Button>
@@ -56,4 +83,5 @@ function App() {
   )
 }
 
-export default App
+
+export default App;
